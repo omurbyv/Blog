29a12ffb811a72150c370b8e6dbb11829fa96040
omurbyv <alp3806@gmail.com>
2015-06-12 21:39:37 +0300

    First Commit Creating Project

---
+++ OmurBlog/OmurBlog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OmurBlog.Startup))]
+namespace OmurBlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
